b95200149814f2fee29c69e3803e6d55680600c8
HAYASHI Kentaro <kenhys@gmail.com>
2011-02-21 19:43:08 +0900

    support toggle plugin status icon in status bar. icon borrowed from src/icons/mail.xpm
    original bzr status:
    revno: 12
    committer: HAYASHI Kentaro <kenhys@gmail.com>
    branch nick: autoforward
    timestamp: Mon 2011-02-21 19:43:08 +0900

--- autoforward.c
+++ autoforward.c
@@ -33,11 +33,14 @@
 #include "utils.h"
 #include "alertpanel.h"
 
+#include "online.xpm"
+#include "offline.xpm"
+
 #include <glib/gi18n-lib.h>
 
 static SylPluginInfo info = {
 	"Auto mail forward Plug-in",
-	"0.2.0",
+	"0.3.0",
 	"HAYASHI Kentaro",
 	"Automatically forwarding mail plug-in for Sylpheed"
 };
@@ -49,6 +52,8 @@ static void exec_autoforward_menu_cb(void);
 
 static GtkWidget *g_plugin_on = NULL;
 static GtkWidget *g_plugin_off = NULL;
+static GtkWidget *g_onoff_switch = NULL;
+static GtkTooltips *g_tooltip = NULL;
 
 void plugin_load(void)
 {
@@ -61,23 +66,30 @@ void plugin_load(void)
     GtkWidget *statusbar = syl_plugin_main_window_get_statusbar();
     GtkWidget *plugin_box = gtk_hbox_new(FALSE, 0);
 
-    g_plugin_on = gtk_label_new(_("AF ON"));
-    g_plugin_off = gtk_label_new(_("AF OFF"));
+    GdkPixbuf* on_pixbuf = gdk_pixbuf_new_from_xpm_data((const char**)online_xpm);
+    g_plugin_on=gtk_image_new_from_pixbuf(on_pixbuf);
+    /*g_plugin_on = gtk_label_new(_("AF ON"));*/
+    
+    GdkPixbuf* off_pixbuf = gdk_pixbuf_new_from_xpm_data((const char**)offline_xpm);
+    g_plugin_off=gtk_image_new_from_pixbuf(off_pixbuf);
+    /*g_plugin_off = gtk_label_new(_("AF OFF"));*/
 
     gtk_box_pack_start(GTK_BOX(plugin_box), g_plugin_on, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(plugin_box), g_plugin_off, FALSE, FALSE, 0);
+    
+    g_tooltip = gtk_tooltips_new();
+    
+    g_onoff_switch = gtk_button_new();
+    gtk_button_set_relief(GTK_BUTTON(g_onoff_switch), GTK_RELIEF_NONE);
+	GTK_WIDGET_UNSET_FLAGS(g_onoff_switch, GTK_CAN_FOCUS);
+	gtk_widget_set_size_request(g_onoff_switch, 20, 20);
 
-    GtkWidget *onoff_switch = gtk_button_new();
-    gtk_button_set_relief(GTK_BUTTON(onoff_switch), GTK_RELIEF_NONE);
-	GTK_WIDGET_UNSET_FLAGS(onoff_switch, GTK_CAN_FOCUS);
-	gtk_widget_set_size_request(onoff_switch, 50, 20);
-
-    gtk_container_add(GTK_CONTAINER(onoff_switch), plugin_box);
-	g_signal_connect(G_OBJECT(onoff_switch), "clicked",
+    gtk_container_add(GTK_CONTAINER(g_onoff_switch), plugin_box);
+	g_signal_connect(G_OBJECT(g_onoff_switch), "clicked",
                      G_CALLBACK(exec_autoforward_menu_cb), mainwin);
-	gtk_box_pack_start(GTK_BOX(statusbar), onoff_switch, FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(statusbar), g_onoff_switch, FALSE, FALSE, 0);
 
-    gtk_widget_show_all(onoff_switch);
+    gtk_widget_show_all(g_onoff_switch);
     gtk_widget_hide(g_plugin_on);
 
 }
@@ -104,11 +116,19 @@ static void exec_autoforward_menu_cb(void)
         g_enable=TRUE;
         gtk_widget_hide(g_plugin_off);
         gtk_widget_show(g_plugin_on);
+        gtk_tooltips_set_tip
+			(g_tooltip, g_onoff_switch,
+			 _("Autoforward is enabled. Click the icon to disable plugin."),
+			 NULL);
     }else{
         syl_plugin_alertpanel_message(_("Autoforward"), _("autoforward plugin is disabled."), ALERT_NOTICE);
         g_enable=FALSE;
         gtk_widget_hide(g_plugin_on);
         gtk_widget_show(g_plugin_off);
+        gtk_tooltips_set_tip
+			(g_tooltip, g_onoff_switch,
+			 _("Autoforward is disabled. Click the icon to enable plugin."),
+			 NULL);
     }
 }
 
