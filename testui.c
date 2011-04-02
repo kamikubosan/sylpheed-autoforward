2c1fa8e38a6e95c86fb7b17a35ff1360b6dcf58c
HAYASHI Kentaro <kenhys@gmail.com>
2011-04-02 19:03:29 +0900

    test on/off treeview.
    original bzr status:
    revno: 28
    committer: HAYASHI Kentaro <kenhys@gmail.com>
    branch nick: autoforward
    timestamp: Sat 2011-04-02 19:03:29 +0900

--- testui.c
+++ testui.c
@@ -1,21 +1,107 @@
 #include <gtk/gtk.h>
 
+static gboolean delete_event( GtkWidget *widget,
+                              GdkEvent  *event,
+                              gpointer   data )
+{
+    g_print ("delete event occurred\n");
+
+    return FALSE;
+}
+
+static void destroy( GtkWidget *widget,
+                     gpointer   data )
+{
+    gtk_main_quit ();
+}
+
+static void add_folder( GtkWidget *widget,
+                     gpointer   data )
+{
+    gtk_main_quit ();
+}
+
+static void forward_all( GtkButton *widget,
+                     gpointer   data )
+{
+  gtk_widget_set_sensitive(GTK_WIDGET(data), FALSE);
+  
+}
+
+static void forward_folder( GtkButton *widget,
+                     gpointer   data )
+{
+  gtk_widget_set_sensitive(GTK_WIDGET(data), TRUE);
+}
+
+static void delete_folder( GtkWidget *widget,
+                     gpointer   data )
+{
+  GtkTreeSelection *selection = gtk_tree_view_get_selection ( GTK_TREE_VIEW(data) );
+   GtkListStore *store;
+   GtkTreeModel *model;
+   GtkTreeIter iter;
+   
+   if (gtk_tree_selection_count_selected_rows(selection) == 0){
+     g_print("no selection\n");
+      return;
+   }
+   GList *list = gtk_tree_selection_get_selected_rows( selection, &model );
+   store = GTK_LIST_STORE ( model );
+   
+   if (!list){
+     g_print("no list\n");
+     return;
+   }
+
+   int nRemoved = 0;
+   while(list) {
+      int ipath = atoi(gtk_tree_path_to_string(list->data));
+      ipath-=nRemoved;
+      GString *fixed_path = g_string_new("");
+      g_string_printf(fixed_path, "%d", ipath);
+      
+      GtkTreePath *path = gtk_tree_path_new_from_string(fixed_path->str);
+      g_string_free(fixed_path, TRUE);
+      
+      if (path) {
+         if ( gtk_tree_model_get_iter ( model, &iter, path) ) { 
+            gtk_list_store_remove ( store, &iter );
+            nRemoved++;   
+         }
+         gtk_tree_path_free (path);
+      }
+      list = list->next;
+   }
+   g_list_foreach (list, (GFunc)gtk_tree_path_free, NULL);
+
+   g_list_free (list);
+}
 int main(int argc, char* argv[])
 {
-    GtkWidget *window, *radio1, *radio2, *box, *entry; 
+  GtkWidget *window, *radio1, *radio2, *box, *entry; 
 
-     gtk_init (&argc, &argv);
+  gtk_init (&argc, &argv);
 
-     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-   box = gtk_vbox_new (TRUE, 2);
-   /* Create a radio button with a GtkEntry widget */
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+
+  g_signal_connect (window, "delete-event",
+                    G_CALLBACK (delete_event), NULL);
+    
+  g_signal_connect (window, "destroy",
+                    G_CALLBACK (destroy), NULL);
+
+  box = gtk_vbox_new (FALSE, 2);
+  /* Create a radio button with a GtkEntry widget */
    radio1 = gtk_radio_button_new_with_label(NULL, "Forward all mail");
    /* Create a radio button with a label */
    radio2 = gtk_radio_button_new_with_label_from_widget (GTK_RADIO_BUTTON (radio1),
                                                          "Forward mail in folder");
+
+   
    /* Pack them into a box, then show all the widgets */
-   gtk_box_pack_start (GTK_BOX (box), radio1, TRUE, TRUE, 2);
-   gtk_box_pack_start (GTK_BOX (box), radio2, TRUE, TRUE, 2);
+   gtk_box_pack_start (GTK_BOX (box), radio1, FALSE, FALSE, 2);
+   gtk_box_pack_start (GTK_BOX (box), radio2, FALSE, FALSE, 2);
 
    /**/
    GtkListStore *store = gtk_list_store_new(1, G_TYPE_STRING);
@@ -46,7 +132,36 @@ int main(int argc, char* argv[])
    gtk_container_add (GTK_CONTAINER (sw), view);
    gtk_box_pack_start (GTK_BOX (box), sw, TRUE, TRUE, 2);
    
-   
+   /**/
+    GtkWidget *bbox = gtk_hbutton_box_new();
+    gtk_button_box_set_layout(GTK_BUTTON_BOX(bbox), GTK_BUTTONBOX_END);
+    gtk_box_set_spacing(GTK_BOX(bbox), 6);
+    GtkWidget *btn_add = gtk_button_new_from_stock(GTK_STOCK_ADD);
+    GtkWidget *btn_delete = gtk_button_new_from_stock(GTK_STOCK_DELETE);
+    GtkWidget *btn_exit = gtk_button_new_from_stock(GTK_STOCK_QUIT);
+
+    g_signal_connect (btn_exit, "clicked",
+		      G_CALLBACK (destroy), NULL);
+       
+    g_signal_connect (btn_add, "clicked",
+		      G_CALLBACK (add_folder), view);
+
+    g_signal_connect (btn_delete, "clicked",
+		      G_CALLBACK (delete_folder), view);
+
+    g_signal_connect(GTK_BUTTON(radio1), "clicked",
+                     G_CALLBACK(forward_all), view);
+
+    g_signal_connect(GTK_BUTTON(radio2), "clicked",
+                     G_CALLBACK(forward_folder), view);
+
+      gtk_box_pack_start(GTK_BOX(bbox), btn_add, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(bbox), btn_delete, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(bbox), btn_exit, FALSE, FALSE, 0);
+    gtk_widget_show_all(bbox);
+    
+    gtk_box_pack_end(GTK_BOX(box), bbox, FALSE, FALSE, 6);
+
    gtk_container_add (GTK_CONTAINER (window), box);
    gtk_widget_show_all (window);
 
